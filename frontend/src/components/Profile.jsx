e8c25f068ea657f6e87d9f319db5971f33463bdb
cjgamble21 <cjgamble21@gmail.com>
2022-04-12 14:58:37 -0500

    Added a static modal

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,15 +1,21 @@
 import { useState, useEffect } from 'react';
 import jwt from 'jwt-decode';
 import { useAuth } from '../hooks/useAuth';
-import { flushSync } from 'react-dom';
+import { Modal, Button } from 'react-bootstrap';
+import { StaticProfileInfo, DynamicProfileInfo } from './ProfileInfo';
 
 
 const Profile = () => {
     const [profile, setProfile] = useState({});
+    const [modal, setModal] = useState(false);
     const { auth } = useAuth();
     // const username = jwt(auth.token).username;
     let username = null;
     // console.log(username);
+
+    // Modal methods
+    const handleShow = () => setModal(true);
+    const handleHide = () => setModal(false);
 
     const vaccines = [
         {
@@ -30,7 +36,8 @@ const Profile = () => {
         first_name: 'Connor',
         last_name: 'Gamble',
         email: 'cjgamble21@gmail.com',
-        age: 21
+        age: 21,
+        organization: 'Southern Methodist University'
     }
 
     return (
@@ -54,7 +61,8 @@ const Profile = () => {
                             <img src="https://via.placeholder.com/150" alt="avatar" className="rounded-circle img-fluid w-3" />
                             <h5 className='my-3'> {user.first_name} {user.last_name} </h5>
                             <p className='text-muted mb-1'>{user.username}</p>
-                            <p className='text-muted mb-4'>{user.email}</p>
+                            <p className='text-muted mb-1'>{user.email}</p>
+                            <p className='mb-4 fw-bold'>{user.organization}</p>
                         </div>
                     </div>
                     <div className='card mb-4 mb-lg-0'>
@@ -79,39 +87,32 @@ const Profile = () => {
                             <h5>User Information</h5>
                         </div>
                         <div className='card-body'>
-                            <div className='row'>
-                                <div className='col-sm-3'>
-                                    <h6 className='mb-0'>Name</h6>
-                                </div>
-                                <div className='col-sm-9'>
-                                    <p className='mb-0 text-muted'>{user.first_name} {user.last_name}</p>
-                                </div>
-                            </div>
+                            <StaticProfileInfo firstName={user.first_name} lastName={user.last_name}
+                                age={user.age} email={user.email} username={user.username} />
                             <hr />
                             <div className='row'>
-                                <div className='col-sm-3'>
-                                    <h6 className='mb-0'>Email</h6>
-                                </div>
-                                <div className='col-sm-9'>
-                                    <p className='mb-0 text-muted'>{user.email}</p>
-                                </div>
-                            </div>
-                            <hr />
-                            <div className='row'>
-                                <div className='col-sm-3'>
-                                    <h6 className='mb-0'>Username</h6>
-                                </div>
-                                <div className='col-sm-9'>
-                                    <p className='mb-0 text-muted'>{user.username}</p>
-                                </div>
-                            </div>
-                            <hr />
-                            <div className='row'>
-                                <div className='col-sm-3'>
-                                    <h6 className='mb-0'>Age</h6>
-                                </div>
-                                <div className='col-sm-9'>
-                                    <p className='mb-0 text-muted'>{user.age}</p>
+                                <div className='col-sm-10'>
+                                    <Button variant="primary" onClick={handleShow}>
+                                        Edit
+                                    </Button>
+
+                                    <Modal show={modal} onHide={handleHide}>
+                                        <Modal.Header closeButton>
+                                            <Modal.Title>Edit Profile</Modal.Title>
+                                        </Modal.Header>
+                                        <Modal.Body>
+                                            <DynamicProfileInfo firstName={user.first_name} lastName={user.last_name}
+                                                age={user.age} email={user.email} username={user.username} />
+                                        </Modal.Body>
+                                        <Modal.Footer>
+                                            <Button variant="secondary" onClick={handleHide}>
+                                                Close
+                                            </Button>
+                                            <Button variant="primary" onClick={handleHide}>
+                                                Save
+                                            </Button>
+                                        </Modal.Footer>
+                                    </Modal>
                                 </div>
                             </div>
                         </div>
