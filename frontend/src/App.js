31156a508b253daee4bb35ac0e5d3bfb54f6fd25
fordjackson26 <77986713+fordjackson26@users.noreply.github.com>
2022-04-25 20:06:27 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,9 +9,9 @@ import { RequireAuth } from './RequireAuth';
 // React functional component
 const App = () => {
   // ENTER YOUR EC2 PUBLIC IP/URL HERE
-  const ec2_url = ''
+  const ec2_url = '54.221.79.200'
   // CHANGE THIS TO TRUE IF HOSTING ON EC2, MAKE SURE TO ADD IP/URL ABOVE
-  const ec2 = false;
+  const ec2 = true;
   // USE localhost OR ec2_url ACCORDING TO ENVIRONMENT
   const url = ec2 ? ec2_url : 'localhost';
 
