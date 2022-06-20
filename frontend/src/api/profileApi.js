a3b1c37fe65891d175f7afe1c2f2bff96986c2e3
cjgamble21 <cjgamble21@gmail.com>
2022-06-20 11:54:36 -0500

    about to merge

---
+++ frontend/src/api/profileApi.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const url = process.env.REACT_APP_API_URL;
+
+const apiConfig = {
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8;application/json'
+}
